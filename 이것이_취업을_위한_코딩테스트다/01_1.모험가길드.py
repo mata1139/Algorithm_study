0571e7b0b3bbb50642dd41fc8a3e32faa2762b53
Minsu Park <pms1139@gmail.com>
2021-08-17 09:41:34 +0900

    Update 01_1.모험가길드.py

--- 이것이_취업을_위한_코딩테스트다/01_1.모험가길드.py
+++ 이것이_취업을_위한_코딩테스트다/01_1.모험가길드.py
@@ -1,16 +1,17 @@
-N = int(input())
-adventure = list(map(int,input().split()))
-answer = 0
+N = int(input()) # N 입력
+adventure = list(map(int,input().split())) # 모험가 리스트 입력
 
-adventure.sort()
-count = 0
+answer = 0 # 최종 그룹 수
 
-for horror in adventure:
-    count += 1
+adventure.sort() # 공포도 순으로 정렬
+count = 0        # 그룹 내 인원 수
 
-    if count >= horror :
-        answer += 1
-        count = 0
+for horror in adventure: # 모험가 전체를 순회,
+    count += 1           # 그룹 인원수++
+
+    if count >= horror : # 그룹 인원수가 현재 모험가 공포도 보다 크거나 같다면,
+        answer += 1      # 그룹 자르기,
+        count = 0        # 인원수 
 
 print(answer)
 
