5332dd8f65a015693d89eccb111a5db518b7ff8e
Minsu Park <pms1139@gmail.com>
2021-08-17 17:54:12 +0900

    Update 01_5.볼링공_고르기.py

--- 이것이_취업을_위한_코딩테스트다/01_5.볼링공_고르기.py
+++ 이것이_취업을_위한_코딩테스트다/01_5.볼링공_고르기.py
@@ -6,7 +6,7 @@ ball_dict = {} # 무게 : 갯수 Dictionary 생성
 result = 0 # 최종 가능한 조합 수
 
 for weight in ball: # Dictionary 초기화 과정,
-    ball_dict[weight] = ball.count(weight)
+    ball_dict[weight] = ball.count(weight) # 공 무게 : 
 
 for count in ball_dict.values(): # 모든 Value 들 즉, 무게 별 공 갯수에 대해,
     N -= count # 현재 무게 갯수를 제외한 공의 갯수를 계산,
