3dd9886f566da87ca87418bd03f5cec9a4ea788e
Minsu Park <pms1139@gmail.com>
2021-08-24 16:56:54 +0900

    Update 02_7.럭키_스트레이트.py

--- 이것이_취업을_위한_코딩테스트다/02_7.럭키_스트레이트.py
+++ 이것이_취업을_위한_코딩테스트다/02_7.럭키_스트레이트.py
@@ -6,6 +6,8 @@ check = 0
 
 
 for idx in range(len(front)): # 왼쪽 부분과 오른쪽 부분 자릿수들의 차의 합이 0이면, LUCKY
+                              # 123 / 402
+                              # (1-4) + (2-0) + (3-2) = 0 → LUCKY
     
     sum = int(front[idx]) - int(rear[idx]) # 각 자릿수의 차
     check += sum                          
